638db14b0d9162c476044cf84731ad86e2021d70
Sannasichottu <sannasichottu@gmail.com>
2022-04-09 20:16:01 +0530

    post method added

--- index.js
+++ index.js
@@ -2,51 +2,68 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { response } from "express";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
+const MONGO_URL = process.env.MONGO_URL;
 
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongodb is connected");
+  return client;
+}
 
-const MONGO_URL = process.env.MONGO_URL
-
-async function createConnection(){
-    const client = new MongoClient(MONGO_URL)
-    await client.connect();
-    console.log("Mongodb is connected")
-    return client;
-} 
-
-const client = await createConnection()
+const client = await createConnection();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.send("Hello Everyone");
+app.get("/", function (req, res) {
+  res.send("Hello Everyone");
 });
 
 /* --- mobiles --- */
-app.get("/mobiles", async function(req, res) {
-    //db.mobiles.find({})
-    const mobiles = await client.db("ecommerce")
+app.get("/mobiles", async function (req, res) {
+  //db.mobiles.find({})
+  const mobiles = await client
+    .db("ecommerce")
     .collection("mobiles")
     .find({})
     .toArray();
 
-
-    res.send(mobiles);
+  res.send(mobiles);
 });
 
 /* --- post --- */
-app.post("/mobiles", async function(req, res) {
-    const data = req.body
-    //db.mobiles.insertMany(data)
-    console.log(data);
-    const result = await client.db("ecommerce").collection("mobiles").insertMany(data)
-    res.send(result);
+app.post("/mobiles", async function (req, res) {
+  const data = req.body;
+  //db.mobiles.insertMany(data)
+  console.log(data);
+  const result = await client
+    .db("ecommerce")
+    .collection("mobiles")
+    .insertMany(data);
+  res.send(result);
 });
 
-app.listen(PORT, () => console.log("App started in 😊", PORT))
+app.put("/cart", async function (req, res) {
+  const mobile = req.body;
+  //db.mobiles.insertMany(data)
+  // if item - updateOne - else - insertOne (qty -1 )
+  console.log(mobile);
+
+  const cartItem = await client
+  .db("ecommerce")
+  .collection("cart")
+  .findOne({ _id: mobile._id });
+  console.log(cartItem);
+
+  res.send(cartItem);
+});
+
+app.listen(PORT, () => console.log("App started in 😊", PORT));
